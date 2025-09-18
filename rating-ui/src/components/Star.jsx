821b7a4799ad7ae581e3844c1b8d454be42626b6
Mapcky <mperano625@gmail.com>
2025-09-18 14:45:02 -0300

    first commit

---
+++ rating-ui/src/components/Star.jsx
@@ -0,0 +1,23 @@
+const Star = ({
+  star,
+  rating,
+  hover,
+  color,
+  ratingClick,
+  hoverEnter,
+  hoverLeave,
+}) => {
+  return (
+    <span
+      className="star"
+      onClick={() => ratingClick(star)}
+      onMouseEnter={() => hoverEnter(star)}
+      onMouseLeave={hoverLeave}
+      style={{ color: star <= (hover || rating) ? color : "#ccc" }}
+    >
+      {"\u2605"}
+    </span>
+  );
+};
+
+export default Star;
