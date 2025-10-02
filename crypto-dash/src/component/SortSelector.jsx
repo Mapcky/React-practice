78bbc2b9f1acb6eb6aaa2191fcda779461fb0a83
Mapcky <mperano625@gmail.com>
2025-10-02 17:29:13 -0300

    api fetch, sort and search

---
+++ crypto-dash/src/component/SortSelector.jsx
@@ -0,0 +1,25 @@
+const SortSelector = ({ sortBy, onSortChange }) => {
+  return (
+    <div className="controls">
+      <label htmlFor="sort">
+        Sort By:
+        <select
+          id="sort"
+          value={sortBy}
+          onChange={(e) => {
+            onSortChange(e.target.value);
+          }}
+        >
+          <option value="market_cap_desc">Market Cap (High To Low)</option>
+          <option value="market_cap_asc">Market Cap (Low To High)</option>
+          <option value="price_desc">Price (High To Low)</option>
+          <option value="price_asc">Market Cap (Low To High)</option>
+          <option value="change_desc">24h Change (High To Low)</option>
+          <option value="change_asc">Market Cap (Low To High)</option>
+        </select>
+      </label>
+    </div>
+  );
+};
+
+export default SortSelector;
