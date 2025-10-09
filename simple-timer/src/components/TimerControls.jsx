0186e5aa45d1e9c6b21507c26d94b53e782eb837
Mapcky <mperano625@gmail.com>
2025-10-09 11:08:31 -0300

    pre build

--- simple-timer/src/components/TimerControls.jsx
+++ simple-timer/src/components/TimerControls.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useRef } from "react";
+
 const TimerControls = ({ isRunning, toggleTimer, resetTimer }) => {
+  const startButtonRef = useRef(null);
+
+  useEffect(() => {
+    if (startButtonRef.current) {
+      startButtonRef.current.focus();
+    }
+  }, []);
+
   return (
     <div>
       <button
+        ref={startButtonRef}
         onClick={toggleTimer}
         className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover: bg-green-600 mr-3"
       >
