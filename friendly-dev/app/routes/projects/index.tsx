839aa066e127ed7e3b9f6dd86a2a251c1baf3c4b
Mapcky <mperano625@gmail.com>
2025-10-16 17:31:38 -0300

    server & client loaders

--- friendly-dev/app/routes/projects/index.tsx
+++ friendly-dev/app/routes/projects/index.tsx
@@ -1,7 +1,28 @@
-const ProjectsPage = () => {
+import ProjectCard from "~/components/ProjectCard";
+import type { Route } from "./+types/index";
+import type { Project } from "~/types";
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch("http://localhost:8000/projects");
+  const data = await res.json();
+
+  return { projects: data };
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData as { projects: Project[] };
+
   return (
     <section>
       <h2 className="text-3xl text-white font-bold mb-8">🚀 Projects</h2>
+
+      <div className="grid gap-6 sm:grid-cols-2">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </section>
   );
 };
