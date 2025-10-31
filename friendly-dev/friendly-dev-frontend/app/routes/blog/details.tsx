bdef38d803aaee15e9e9404ca24d946aa424bab3
Mapcky <mperano625@gmail.com>
2025-10-31 16:46:27 -0300

    form changes & setting up new project

--- friendly-dev/friendly-dev-frontend/app/routes/blog/details.tsx
+++ friendly-dev/friendly-dev-frontend/app/routes/blog/details.tsx
@@ -29,9 +29,7 @@ export async function loader({ request, params }: Route.LoaderArgs) {
     slug: item.slug,
     excerpt: item.excerpt,
     body: item.body,
-    image: item.image?.url
-      ? `${import.meta.env.VITE_STRAPI_URL}${item.image.url}`
-      : "images/no-image.png",
+    image: item.image?.url ? `${item.image.url}` : "images/no-image.png",
     date: item.date,
   };
 
--- friendly-dev/friendly-dev-frontend/app/routes/contact/index.tsx
+++ friendly-dev/friendly-dev-frontend/app/routes/contact/index.tsx
@@ -1,50 +1,17 @@
 import type { Route } from "./+types";
-import { Form } from "react-router";
-
-export async function action({ request }: Route.ActionArgs) {
-  const formData = await request.formData();
-  const name = formData.get("name") as string;
-  const email = formData.get("email") as string;
-  const subject = formData.get("subject") as string;
-  const message = formData.get("message") as string;
-
-  const errors: Record<string, string> = {};
-
-  if (!name) errors.name = "Name is required";
-  if (!email) {
-    errors.email = "Email is required";
-  } else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
-    errors.email = "Invalid email format";
-  }
-
-  if (!subject) errors.subject = "Subject is required";
-
-  if (!message) errors.message = "Message is required";
-
-  if (Object.keys(errors).length > 0) {
-    return { errors };
-  }
-
-  const data = { name, email, subject, message };
-
-  return { message: "Form submitted successfully", data };
-}
 
 const ContactPage = ({ actionData }: Route.ComponentProps) => {
-  const errors = actionData?.errors || {};
-
   return (
     <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
       <h2 className="text-3xl font-bold text-white mb-8 text-center">
         📫 Contact Me
       </h2>
 
-      {actionData?.message ? (
-        <p className="mb-6 p-2 bg-green-700 text-green-100 text-center rounded-lg border border-green-500 shadow-md">
-          {actionData.message}
-        </p>
-      ) : null}
-      <Form method="post" className="space-y-6">
+      <form
+        action="https://formspree.io/f/xrboyvdr"
+        method="post"
+        className="space-y-6"
+      >
         <div>
           <label
             htmlFor="name"
@@ -58,9 +25,6 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
             name="name"
             className="w-full mt-1 px-4 py-2 border border-gray-700 rounded lg bg-gray-800 text-gray-100"
           />
-          {errors.name && (
-            <p className="text-red-400 text-sm mt-1">{errors.name}</p>
-          )}
         </div>
         <div>
           <label
@@ -76,9 +40,6 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
             className="w-full mt-1 px-4 py-2 border border-gray-700 rounded lg bg-gray-800 text-gray-100"
           />
         </div>
-        {errors.email && (
-          <p className="text-red-400 text-sm mt-1">{errors.email}</p>
-        )}
 
         <div>
           <label
@@ -94,9 +55,7 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
             className="w-full mt-1 px-4 py-2 border border-gray-700 rounded lg bg-gray-800 text-gray-100"
           />
         </div>
-        {errors.subject && (
-          <p className="text-red-400 text-sm mt-1">{errors.subject}</p>
-        )}
+
         <div>
           <label
             htmlFor="message"
@@ -110,13 +69,11 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
             className="w-full mt-1 px-4 py-2 border border-gray-700 rounded lg bg-gray-800 text-gray-100"
           />
         </div>
-        {errors.message && (
-          <p className="text-red-400 text-sm mt-1">{errors.message}</p>
-        )}
+
         <button className="w-full text-white py-2 rounded-lg bg-blue-600 hover:bg-blue-700 cursor-pointer">
           Send Message
         </button>
-      </Form>
+      </form>
     </div>
   );
 };
